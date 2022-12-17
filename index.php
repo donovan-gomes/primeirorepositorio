3174e8e51da69bb56e70131fcf9b178964631189
donovan-gomes <120754773+donovan-gomes@users.noreply.github.com>
2022-12-16 21:26:19 -0300

    Inicializar o repositório com o arquivo index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Página Inicial</title>
+</head>
+<body>
+  <header>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Contato</a>
+    </nav>
+  </header>
+  <section>
+    <?php 
+      echo "Primeiro repositorio."
+    ?>
+  </section>
+  <footer>
+    <h4>Desenvolvido por DnSistemas</h4>
+  </footer>
+</body>
+</html>
